d88bc2cd318dcfde16ed6f9033035a47fc46cb74
Sebastien Mesili <leseb77@hotmail.com>
2020-08-16 21:53:24 +0900

    Added cart demo

---
+++ cart/precache-manifest.c21fa26ecff7e5c51f49962c4e544618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13d2472a8ca7df26442ef4fd8da6c1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98bc7d2c24f8de8a53b4",
+    "url": "/static/css/main.afa98f87.chunk.css"
+  },
+  {
+    "revision": "a8e892061a5ac2e5e873",
+    "url": "/static/js/2.605ed12d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.605ed12d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98bc7d2c24f8de8a53b4",
+    "url": "/static/js/main.d12dc13e.chunk.js"
+  },
+  {
+    "revision": "13cf3d3d774fda3aae31",
+    "url": "/static/js/runtime-main.1daf25c3.js"
+  }
+]);
