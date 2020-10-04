d176c4ca21e0403f6c429079d611d34f372a48a6
Sebastien Mesili <leseb77@hotmail.com>
2020-10-04 16:17:31 +0900

    Added starblast-serverlist

---
+++ starblast-serverlist/precache-manifest.c6e0590dfecbf01c5cba108fc311c5eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f191190e5c1b18c5285a795e2504e9fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0fc477f8e94ba954422",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0eb6d2b2f0085820473c",
+    "url": "/static/css/main.b57f1b7a.chunk.css"
+  },
+  {
+    "revision": "a0fc477f8e94ba954422",
+    "url": "/static/js/2.009f9fce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.009f9fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb6d2b2f0085820473c",
+    "url": "/static/js/main.951d58f7.chunk.js"
+  },
+  {
+    "revision": "13a7744823a70f5baf4d",
+    "url": "/static/js/runtime-main.2f44effa.js"
+  }
+]);
